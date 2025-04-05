084184d0a1867a09b67376d2bb8429c99ffe163a
leedahoon <ekgns0508@gmail.com>
2025-04-05 16:03:15 +0900

    feat: ExceptionHandler 클래스 생성
    - CustomException에 대한 ExceptionHandler 설정
    
    FDORA-129

---
+++ src/main/java/com/farmdora/farmdora/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.farmdora.farmdora.common.exception;
+
+import com.farmdora.farmdora.common.response.HttpResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Configuration
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<?> customExceptionHandler(CustomException e) {
+        return ResponseEntity
+                .status(e.getStatus())
+                .body(new HttpResponse(e.getStatus(), e.getMessage(), null));
+    }
+}
