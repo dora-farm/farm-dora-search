53a7fe9109c065776386065a3101be5e62af4a63
leedahoon <ekgns0508@gmail.com>
2025-04-15 12:27:21 +0900

    feat: 상품 상세 조회 성공메시지 추가
    FDORA-34

--- src/main/java/com/farmdora/farmdora/common/response/SuccessMessage.java
+++ src/main/java/com/farmdora/farmdora/common/response/SuccessMessage.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SuccessMessage {
     SEARCH_ORDER_SUCCESS("주문 목록 조회에 성공하였습니다."),
-    SEARCH_SALES_SUCCESS("상품 목록 조회에 성공하였습니다.");
+    SEARCH_SALES_SUCCESS("상품 목록 조회에 성공하였습니다."),
+    GET_SALE_DETAIL_SUCCESS("상품 상세 조회에 성공하였습니다.");
 
     private final String message;
 }
