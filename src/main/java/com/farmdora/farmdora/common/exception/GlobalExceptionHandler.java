97221bd3dc1b581330ce8271ecfd510ae7e94f01
leedahoon <ekgns0508@gmail.com>
2025-04-16 10:54:27 +0900

    feat: ExceptionHandler 추가
    - IllegalArgumentStatementException 예외 처리 설정 추가
    
    FDORA-144

--- src/main/java/com/farmdora/farmdora/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/farmdora/farmdora/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.farmdora.farmdora.common.exception;
 
 import com.farmdora.farmdora.common.response.HttpResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,10 +9,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final String ILLEGAL_ERROR_MESSAGE = "서버 에러가 발생했습니다.";
+
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<?> customExceptionHandler(BaseException e) {
         return ResponseEntity
                 .status(e.getStatus())
                 .body(new HttpResponse(e.getStatus(), e.getMessage(), null));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> customExceptionHandler(IllegalArgumentException e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new HttpResponse(HttpStatus.INTERNAL_SERVER_ERROR, ILLEGAL_ERROR_MESSAGE, null));
     }
 }
