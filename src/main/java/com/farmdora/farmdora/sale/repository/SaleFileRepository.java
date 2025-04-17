6919d6ed6e4d71bdd4b94bbe0f251fb17966a2e7
leedahoon <ekgns0508@gmail.com>
2025-04-17 11:39:56 +0900

    feat: 상품의 메인 이미지 조회 쿼리메서드 추가
    FDORA-35

--- src/main/java/com/farmdora/farmdora/sale/repository/SaleFileRepository.java
+++ src/main/java/com/farmdora/farmdora/sale/repository/SaleFileRepository.java
@@ -3,8 +3,10 @@ package com.farmdora.farmdora.sale.repository;
 import com.farmdora.farmdora.entity.Sale;
 import com.farmdora.farmdora.entity.SaleFile;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SaleFileRepository extends JpaRepository<SaleFile, Integer> {
     List<SaleFile> findAllBySale(Sale sale);
+    Optional<SaleFile> findBySaleIdAndIsMainIsTrue(Integer saleId);
 }
