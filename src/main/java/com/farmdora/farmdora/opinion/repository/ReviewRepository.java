c53a72dfc8238efe650ca8c0e77612076704a544
leedahoon <ekgns0508@gmail.com>
2025-04-18 12:00:24 +0900

    refactor: 쿼리메서드 수정
    - saleId -> Sale 엔티티로 조회
    
    FDORA-36

--- src/main/java/com/farmdora/farmdora/opinion/repository/ReviewRepository.java
+++ src/main/java/com/farmdora/farmdora/opinion/repository/ReviewRepository.java
@@ -1,10 +1,11 @@
 package com.farmdora.farmdora.opinion.repository;
 
 import com.farmdora.farmdora.entity.Review;
+import com.farmdora.farmdora.entity.Sale;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer>, CustomReviewRepository {
-    Page<Review> findAllBySaleId(Integer saleId, Pageable pageable);
+    Page<Review> findAllBySale(Sale sale, Pageable pageable);
 }
