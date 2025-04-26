073e86535bb2524f5c166e1f87ca899c74173382
leedahoon <ekgns0508@gmail.com>
2025-04-26 23:24:20 +0900

    feat: 사용자 검색 API 테스트 케이스 추가
    FDORA-146

---
+++ src/test/java/com/farmdora/farmdora/admin/controller/UserControllerTest.java
@@ -0,0 +1,55 @@
+package com.farmdora.farmdora.admin.controller;
+
+import static com.farmdora.farmdora.common.response.SuccessMessage.SEARCH_USERS_SUCCESS;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.farmdora.farmdora.ControllerTest;
+import com.farmdora.farmdora.admin.dto.UserSearchRequestDto;
+import com.farmdora.farmdora.admin.dto.UserSearchResponseDto;
+import com.farmdora.farmdora.common.response.PageResponseDto;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Pageable;
+
+class UserControllerTest extends ControllerTest {
+
+    @Test
+    @DisplayName("관리자의 사용자 목록 검색 API 테스트")
+    void testSearchUsers() throws Exception {
+        // given
+        List<UserSearchResponseDto> users = List.of(
+                UserSearchResponseDto.builder()
+                        .userId(1)
+                        .isBlind(true)
+                        .name("user1")
+                        .isSeller(false)
+                        .build(),
+                UserSearchResponseDto.builder()
+                        .userId(2)
+                        .isBlind(true)
+                        .name("user2")
+                        .isSeller(true)
+                        .build()
+        );
+        PageResponseDto<UserSearchResponseDto> pageResult = new PageResponseDto<>();
+        pageResult.setContents(users);
+        pageResult.setTotalElements(2L);
+        when(userService.searchUsers(any(UserSearchRequestDto.class), any(Pageable.class))).thenReturn(pageResult);
+
+        // when
+        // then
+        mvc.perform(get("/admin/user")
+                .param("keyword", "user")
+                .param("types", "USER"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status", equalTo(200)))
+                .andExpect(jsonPath("$.message", equalTo(SEARCH_USERS_SUCCESS.getMessage())))
+                .andExpect(jsonPath("$.data.contents.size()", equalTo(2)));
+    }
+}
