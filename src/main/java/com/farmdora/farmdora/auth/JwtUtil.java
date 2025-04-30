a4d01a3d8e94cfdc75ea956d08400fe70791baaf
leedahoon <ekgns0508@gmail.com>
2025-04-30 14:41:32 +0900

    feat: JWT 토큰 검사 유틸클래스 추가
    FDORA-154

---
+++ src/main/java/com/farmdora/farmdora/auth/JwtUtil.java
@@ -0,0 +1,61 @@
+package com.farmdora.farmdora.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JwtUtil {
+
+    private final SecretKey secretKey;
+
+    public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
+                Jwts.SIG.HS512.key()
+                        .build()
+                        .getAlgorithm());
+    }
+
+    public Integer getUserId(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload().get("userId", Integer.class);
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities(String token) {
+        String role = Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("role", String.class);
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    public boolean validateToken(String token) {
+        try{
+            Claims claims = Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token).getPayload();
+            return !claims.getExpiration().before(new Date());
+        } catch (JwtException | IllegalArgumentException e) {
+            log.error("Jwt 요효성 검사 실패: {}" , e.getMessage());
+            return false;
+        }
+    }
+}
