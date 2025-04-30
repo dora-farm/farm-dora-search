39706e23facec740097411dd6560b9f15f435424
leedahoon <ekgns0508@gmail.com>
2025-04-30 14:42:11 +0900

    feat: IllegalArgumentException 예외 메시지 로그 출력
    FDORA-154

--- src/main/java/com/farmdora/farmdora/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/farmdora/farmdora/common/exception/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.farmdora.farmdora.common.exception;
 
 import com.farmdora.farmdora.common.response.HttpResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -20,6 +22,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<?> customExceptionHandler(IllegalArgumentException e) {
+        log.error(e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new HttpResponse(HttpStatus.INTERNAL_SERVER_ERROR, ILLEGAL_ERROR_MESSAGE, null));
