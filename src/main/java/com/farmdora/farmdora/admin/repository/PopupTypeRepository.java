c3d34f0df71419d0c086f9f05bbf23d3bd32cd03
leedahoon <ekgns0508@gmail.com>
2025-05-02 10:20:14 +0900

    feat: 팝업 타입 repository 생성
    FDORA-149

---
+++ src/main/java/com/farmdora/farmdora/admin/repository/PopupTypeRepository.java
@@ -0,0 +1,8 @@
+package com.farmdora.farmdora.admin.repository;
+
+import com.farmdora.farmdora.entity.PopupType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PopupTypeRepository extends JpaRepository<PopupType, Short> {
+
+}
