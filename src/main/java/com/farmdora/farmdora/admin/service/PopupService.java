5c8d3e29ee490c34601d697f7d23ef2509f6d2b4
leedahoon <ekgns0508@gmail.com>
2025-05-02 10:20:26 +0900

    feat: 팝업 타입 목록 조회 서비스 로직 구현
    FDORA-149

--- src/main/java/com/farmdora/farmdora/admin/service/PopupService.java
+++ src/main/java/com/farmdora/farmdora/admin/service/PopupService.java
@@ -2,11 +2,15 @@ package com.farmdora.farmdora.admin.service;
 
 import com.farmdora.farmdora.admin.dto.PopupSearchRequestDto;
 import com.farmdora.farmdora.admin.dto.PopupSearchResponseDto;
+import com.farmdora.farmdora.admin.dto.PopupTypeDto;
 import com.farmdora.farmdora.admin.repository.PopupRepository;
+import com.farmdora.farmdora.admin.repository.PopupTypeRepository;
 import com.farmdora.farmdora.common.NcpImageProperties;
 import com.farmdora.farmdora.common.response.PageResponseDto;
 import com.farmdora.farmdora.entity.Popup;
+import com.farmdora.farmdora.entity.PopupType;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class PopupService {
     private final PopupRepository popupRepository;
+    private final PopupTypeRepository popupTypeRepository;
     private final NcpImageProperties ncpImageProperties;
 
     @Transactional(readOnly = true)
@@ -28,5 +33,13 @@ public class PopupService {
                         ncpImageProperties.getType()))
                 .toList();
         return new PageResponseDto<>(popupSearchResponses, popups);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PopupTypeDto> getPopupTypes() {
+        List<PopupType> popupTypes = popupTypeRepository.findAll();
+        return popupTypes.stream()
+                .map(PopupTypeDto::fromEntity)
+                .collect(Collectors.toList());
     }
 }
