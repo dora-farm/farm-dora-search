9900d09eaa9e6bda14bc949e530e06da8e0438a7
leedahoon <ekgns0508@gmail.com>
2025-05-04 17:05:35 +0900

    fix: 인기상품목록 캐싱 및 조회 로직 수정
    FDORA-157

--- src/main/java/com/farmdora/farmdora/sale/service/SaleRedisService.java
+++ src/main/java/com/farmdora/farmdora/sale/service/SaleRedisService.java
@@ -2,8 +2,10 @@ package com.farmdora.farmdora.sale.service;
 
 import com.farmdora.farmdora.sale.dto.SaleRankingDto;
 import com.farmdora.farmdora.sale.repository.SaleRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Duration;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -20,10 +22,11 @@ public class SaleRedisService {
 
     private final SaleRepository saleRepository;
     private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper om;
 
     private static final int SEARCH_SIZE = 50;
     private static final int PAGE_SIZE = 10;
-    private static final int POPULAR_SALES_DURATION = 10;
+    private static final int POPULAR_SALES_DURATION = 30;
     private static final String POPULAR_SALES_KEY = "popular:sales:page:";
     private static final String POPULAR_SALES_COUNT_KEY = "popular:sales:count";
 
@@ -33,10 +36,9 @@ public class SaleRedisService {
 
         Pageable pageable = PageRequest.of(0, SEARCH_SIZE);
         Page<SaleRankingDto> sales = saleRepository.findTop50ByOrderCount(pageable);
-        redisTemplate.opsForValue().set(POPULAR_SALES_COUNT_KEY, sales.getTotalElements(), Duration.ofMinutes(10));
-
         List<SaleRankingDto> content = sales.getContent();
         int total = content.size();
+        redisTemplate.opsForValue().set(POPULAR_SALES_COUNT_KEY, total, Duration.ofMinutes(10));
 
         for (int page = 0; page < (total + PAGE_SIZE - 1) / PAGE_SIZE; page++) {
             int start = page * PAGE_SIZE;
@@ -51,7 +53,15 @@ public class SaleRedisService {
     }
 
     public List<SaleRankingDto> findSaleRanks(int page) {
-        return (List<SaleRankingDto>) redisTemplate.opsForValue().get(POPULAR_SALES_COUNT_KEY + (page + 1));
+        log.info("인기 상품 목록을 캐시에서 조회합니다.");
+
+        Object cachedSalesValue = redisTemplate.opsForValue().get(POPULAR_SALES_KEY + page);
+        if (cachedSalesValue == null) return null;
+
+        List<?> cachedSales = (List<?>) cachedSalesValue;
+        return cachedSales.stream()
+                .map(obj -> om.convertValue(obj, SaleRankingDto.class))
+                .collect(Collectors.toList());
     }
 
     public Integer findSaleRankCount() {
