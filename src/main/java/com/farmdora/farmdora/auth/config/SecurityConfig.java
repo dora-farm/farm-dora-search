2e0a88f08295db95416e802c178e00ba064cdd64
kisuk <cks71714@naver.com>
2025-05-07 16:37:42 +0900

    add: 도메인 security 추가

--- src/main/java/com/farmdora/farmdora/auth/config/SecurityConfig.java
+++ src/main/java/com/farmdora/farmdora/auth/config/SecurityConfig.java
@@ -58,7 +58,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "http://www.farm-dora.kro.kr",
+                "http://farm-dora.kro.kr"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
