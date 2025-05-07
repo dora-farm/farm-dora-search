0c9d92e78f5b6457e1d15483372cb9ea78d50ae5
kisuk <cks71714@naver.com>
2025-05-07 16:15:24 +0900

    test: 서버 카테고리 조회 경로 수정

--- src/main/java/com/farmdora/farmdora/category/controller/CategoryController.java
+++ src/main/java/com/farmdora/farmdora/category/controller/CategoryController.java
@@ -10,9 +10,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/search")
 @RequiredArgsConstructor
 public class CategoryController {
     private final CategoryService categoryService;
